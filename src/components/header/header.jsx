89d91364304df10153f6d2639324e7f2beee7221
sonnyenrico <aslanzhartybaevstudent@gmail.com>
2023-12-13 12:17:05 +0600

    bl

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import img from './chel.svg';
 
 export default function Header() {
+  const [isDropdownOpen1, setDropdownOpen1] = useState(false);
+  const [isDropdownOpen2, setDropdownOpen2] = useState(false);
+
+  const toggleDropdown1 = () => {
+    setDropdownOpen1(!isDropdownOpen1);
+  };
+
+  const toggleDropdown2 = () => {
+    setDropdownOpen2(!isDropdownOpen2);
+  };
+
   return (
     <div className='header'>
       <div className="container">
         <div className="header-chart">
           <div className="header-inner">
-            <div className="logo">LOGO</div>
+            <a href="#!" className="logo">LOGO</a>
             <a href="#!" className="nav__link1"></a>
             <nav className="navbar">
-              <a href="#!" className="nav__link">MENU</a>
-              <a href="#!" className="nav__link">MENU</a>
-              <a href="#!" className="nav__link">CLUBS</a>
+              <div className="nav__link" onMouseEnter={toggleDropdown1} onMouseLeave={toggleDropdown1}>
+                MENU
+                {isDropdownOpen1 && (
+                  <div className="dropdown-menu">
+                    <a href="#!">Item 1</a>
+                    <a href="#!">Item 2</a>
+                    <a href="#!">Item 3</a>
+                  </div>
+                )}
+              </div>
+              <a href="#!" className="nav__link" onMouseEnter={toggleDropdown2} onMouseLeave={toggleDropdown2}>
+                MENU
+                {isDropdownOpen2 && (
+                  <div className="dropdown-menu">
+                    <a href="#!">Item 1</a>
+                    <a href="#!">Item 2</a>
+                    <a href="#!">Item 3</a>
+                  </div>
+                )}
+              </a>
+              <a href="#!" id='line-1' className="nav__link">CLUBS</a>
             </nav>
           </div>
           <div className="header-inner2">
             <nav className='navbar2'>
-              <a id='line' href="#!" className='nav__link2'>LOGIN | REGISTER</a>
+              <a id='line-2' href="#!" className='nav__link2'>LOGIN | REGISTER</a>
               <a href="#!" className='nav__link2'><img src={img} alt="" /></a>
             </nav>
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,10 +1,87 @@
 import React from 'react';
 import './profile.css';
+import logo from './profile-logo.png'
 
 export default function Profile() {
-    return(
-        <div className="profile-profile">
-            
+    return (
+        <div className="profile">
+            <div className="container">
+                <div className="main">
+                    <div className="profile-text">
+                        <div className="text1">
+                            <a href="#">ГЛАВНАЯ / ПРОФИЛЬ</a>
+                        </div>
+                        <div className="text2">ПРОФИЛЬ</div>
+                    </div>
+                    <div className="profile-main">
+                        <div className="avatar-menu">
+                            <div className="avatar">
+                                <img src={logo} alt="" />
+                            </div>
+                            <div className="avatar-text">
+                                <div className="name-text">ASLAN ZHARTYBAEV</div>
+                                <div className="uni-text">ASTANA IT COLLEGE</div>
+                            </div>
+                        </div>
+                        <div className="info-main">
+                            <div className="info-menu">
+                                <div className="info-card">
+                                    <div className="base-text">Адрес электронной почты</div>
+                                    <div className="gmail-text">
+                                        <a href="#">student@gmail.com</a>
+                                    </div>
+                                </div>
+                                <div className="info-card">
+                                    <div className="base-text">Дата рождения</div>
+                                    <div className="info-text">12.12.2006</div>
+                                </div>
+                                <div className="info-card">
+                                    <div className="base-text">Адрес</div>
+                                    <div className="info-text">Астана</div>
+                                </div>
+                            </div>
+                            <div className="edit-menu">
+                                <div className="btns">
+                                    <div className="btn-edit">
+                                        <a href="#">Edit </a>
+                                    </div>
+                                    <div className="btn-grades">
+                                        <a href="#">Grades </a>
+                                    </div>
+                                </div>
+                                <div className="update-menu">
+                                    <div className="update-text">Profile update</div>
+                                    <div className="edit-input">
+                                        <div className="edit-text">Phone Number</div>
+                                        <div className="input">
+                                            <input type="text" placeholder='Enter phone number' />
+                                        </div>
+                                    </div>
+                                    <div className="edit-input">
+                                        <div className="edit-text">Email</div>
+                                        <div className="input">
+                                            <input type="text" placeholder='Enter email' />
+                                        </div>
+                                    </div>
+                                    <div className="update-text">Change Password</div>
+                                    <div className="edit-input">
+                                        <div className="edit-text">Password</div>
+                                        <div className="input">
+                                            <input type="text" placeholder='Enter password' />
+                                        </div>
+                                    </div>
+                                    <div className="edit-input">
+                                        <div className="edit-text">Confirm password</div>
+                                        <div className="input">
+                                            <input type="text" placeholder='Renter password' />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
